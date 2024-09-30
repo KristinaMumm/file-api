9b987d22a01904fc203bff3abaffabe7438b1566
KristinaMumm <mummkristina@gmail.com>
2024-09-30 20:54:03 +0300

    change: token deleted after file deleted

--- src/main/kotlin/com/hrblizz/fileapi/controller/filecontroller/FileDeleteController.kt
+++ src/main/kotlin/com/hrblizz/fileapi/controller/filecontroller/FileDeleteController.kt
@@ -1,8 +1,10 @@
 package com.hrblizz.fileapi.controller.filecontroller
 
+import com.hrblizz.fileapi.data.entities.Entity
 import com.hrblizz.fileapi.data.repository.EntityRepository
 import com.hrblizz.fileapi.rest.ErrorMessage
 import com.hrblizz.fileapi.rest.ResponseEntity
+import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,15 +18,23 @@ class FileDeleteController(
 ) {
     @RequestMapping("/file/{fileToken}", method = [RequestMethod.DELETE])
     fun deleteFile(@PathVariable fileToken: String): ResponseEntity<Map<String, Any>>? {
-        val fileEntity = fileRepository.findByToken(fileToken)
+        val fileEntity : Entity
+
+        try {
+            fileEntity = fileRepository.findByToken(fileToken)
+        } catch (e: EmptyResultDataAccessException) {
+            val errorMessage = listOf(ErrorMessage("Token not found"))
+            return ResponseEntity(null, errorMessage, HttpStatus.NOT_FOUND.value())
+        }
+
         val file = File(FileControllerConstants.FILES_DIRECTORY, fileEntity.fileName)
 
-        return if (file.exists()) {
+        if (file.exists()) {
             file.delete()
-            ResponseEntity(HttpStatus.OK.value())
-        } else {
-            val errorMessage = listOf(ErrorMessage("File not found"))
-            ResponseEntity(null, errorMessage, HttpStatus.NOT_FOUND.value())
         }
+
+        fileRepository.delete(fileEntity)
+
+        return ResponseEntity(HttpStatus.OK.value())
     }
 }
